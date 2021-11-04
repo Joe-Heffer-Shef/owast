e9ce27d892c35764c3c267e4c7eef3d12ecc770e
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-11-04 13:01:22 +0000

    add notes re file upload in JS

--- ingress/app_factory.py
+++ ingress/app_factory.py
@@ -4,4 +4,6 @@ import flask
 def create_app() -> flask.Flask:
     app = flask.Flask(__name__)
 
+    # TODO LDAP authentication (Active Directory)
+
     return app
--- ingress/blueprints/experiment/views.py
+++ ingress/blueprints/experiment/views.py
@@ -5,4 +5,17 @@ blueprint = flask.Blueprint('experiment', __name__)
 
 @blueprint.route('/create')
 def create():
+    """
+    Write new experiment metadata and file upload.
+    """
+
+    # TODO create SAS token for Azure Blob Storage
+    # this will be passed to Javascript for temporary authentication
+
+    # TODO File upload
+    # https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-nodejs
+
+    # Uploading Large Files in Windows Azure Blob Storage Using Shared Access Signature, HTML, and JavaScript
+    # https://docs.microsoft.com/en-us/answers/questions/535512/how-to-upload-large-files-in-chunks-in-azure-blobs.html
+
     return flask.render_template('experiment/create.html')
