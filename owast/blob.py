52b6865c1555e2a5bb781f806cf425e7271be072
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-11-25 15:27:47 +0000

    add status page

--- owast/blob.py
+++ owast/blob.py
@@ -2,16 +2,18 @@
 Azure Blob Storage
 """
 
+import os
+
 import azure.storage.blob
 
 # Default Azurite credentials
 # https://github.com/Azure/Azurite#connection-strings
-BLOB_ENDPOINT = 'http://blob:10000/devstoreaccount1'
+BLOB_ENDPOINT = os.getenv('BLOB_ENDPOINT',
+                          'http://localhost:10000/devstoreaccount1')
 CONN_STR = f'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint={BLOB_ENDPOINT};'
 
 
 def get_service_client(conn_str: str = None, *args,
-                       **client_kwargs) -> azure.storage.blob.BlobServiceClient:
-    conn_str = conn_str or CONN_STR
+                       **kwargs) -> azure.storage.blob.BlobServiceClient:
     return azure.storage.blob.BlobServiceClient.from_connection_string(
-        conn_str=conn_str, *args, **client_kwargs)
+        conn_str=conn_str or CONN_STR, *args, **kwargs)
--- owast/blueprints/service/views.py
+++ owast/blueprints/service/views.py
@@ -1,7 +1,6 @@
 import os
 
 import flask
-import pymongo.client_session
 import pymongo.errors
 
 app = flask.current_app
@@ -9,25 +8,22 @@ blueprint = flask.Blueprint('service', __name__, url_prefix='/service',
                             template_folder='templates')
 
 
-def ping_mongodb(serverSelectionTimeoutMS: int = 1000, host=None,
+def ping_mongodb(timeout: int = 1000, host=None,
                  **mongo_client_kwargs):
     """
     Is MongoDB up?
     """
     try:
         with pymongo.MongoClient(host or os.environ['MONGO_URI'],
-                                 serverSelectionTimeoutMS=serverSelectionTimeoutMS,
+                                 serverSelectionTimeoutMS=timeout,
                                  **mongo_client_kwargs) as client:
             return client.server_info()
-    except pymongo.errors.ServerSelectionTimeoutError:
-        return
+    except pymongo.errors.ServerSelectionTimeoutError as exc:
+        return exc
 
 
 def ping_blob():
-    import owast.blob
-
-    client = owast.blob.get_service_client()
-    return client.get_service_properties(timeout=1)
+    return
 
 
 @blueprint.route('/')
