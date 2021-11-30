0cef48648758f841e25df4dd01df6ea6167e0bea
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-11-30 16:07:39 +0000

    add research object json schema funcitonality

--- owast/blueprints/thing/views.py
+++ owast/blueprints/thing/views.py
@@ -3,6 +3,7 @@ import json
 import flask
 import pymongo.results
 from bson.objectid import ObjectId
+import bson.json_util
 
 app = flask.current_app
 blueprint = flask.Blueprint('thing', __name__,
@@ -19,23 +20,33 @@ def list_():
 
 @blueprint.route('/create', methods={'GET', 'POST'})
 def create():
+    """
+    Add a new research object
+    """
+    # Process form submisson
     if flask.request.method == 'POST':
+        # Build research object from user input
         thing = {
-            '$schema': 'https://json-schema.org/draft/2020-12/schema',
-            '$id': 'https://localhost/thing/{thing_id}/schema.json',
             'title': '',
             'description': '',
             'type': 'object',
         }
-        _thing = json.loads(flask.request.form['schema'])
-        thing.update(flask.request.form)
+        thing.update(json.loads(flask.request.form['_schema']))
+        thing.update({k: v for k, v in flask.request.form.items() if
+                      not k.startswith('_')})
+
+        # Create document
         result = app.mongo.db.things.insert_one(
             thing)  # type:pymongo.results.InsertOneResult
+
         app.logger.info(result.acknowledged)
         flask.flash(f'Created "{result.inserted_id}"')
+
+        # Redirect to the new obhect
         return flask.redirect(flask.url_for('thing.detail',
                                             thing_id=result.inserted_id))
 
+    # Show form
     return flask.render_template('thing/create.html')
 
 
@@ -46,6 +57,19 @@ def detail(thing_id: ObjectId):
 
 
 @blueprint.route('/<ObjectId:thing_id>/schema.json')
-def detail_json(thing_id: ObjectId):
+def schema(thing_id: ObjectId):
+    """
+    Show the JSON schema document
+    """
     thing = app.mongo.db.things.find_one_or_404(thing_id)
-    return flask.jsonify(thing)
+    schema = dict(
+        **{
+            '$schema': 'https://json-schema.org/draft/2020-12/schema',
+            '$id': f'https://localhost/thing/{thing_id}/schema.json',
+        },
+        **{key: value for key, value in thing.items() if
+           not key.startswith('_')}
+    )
+    return app.response_class(
+        bson.json_util.dumps(schema, **flask.request.args),
+        mimetype='application/json')
