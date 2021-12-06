410fcee99e46c889fde54866c3a76ac8d5c2f58c
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-12-06 17:17:41 +0000

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>2.88.1"
     }
   }
@@ -12,6 +12,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "OWA"
+  name     = "OWA"
   location = "uksouth"
 }
